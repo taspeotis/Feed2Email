0504c72bd7d62cc086d7c9c3d1b8f50a075f63cc
Todd Aspeotis <t@speot.is>
2013-06-30 20:03:41 +1000

    Add skeleton solution structure

---
+++ Feed2Email/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Feed2Email
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Feed2Email/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Feed2Email.MvcApplication" Language="C#" %>
