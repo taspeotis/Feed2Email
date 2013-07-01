e7f7dc58864c683ca16610659e18f50ec423151e
Todd Aspeotis <t@speot.is>
2013-07-01 22:28:42 +1000

    Adding Adminia Admin theme

--- Feed2Email/Global.asax.cs
+++ Feed2Email/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -12,7 +9,7 @@ namespace Feed2Email
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -22,6 +19,13 @@ namespace Feed2Email
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            // http://stackoverflow.com/a/4945950/242520
+            if (!Request.IsLocal && !Request.IsSecureConnection)
+                Response.Redirect(Request.Url.ToString().Replace("http:", "https:"));
         }
     }
 }
