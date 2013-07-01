0f31bfcaca2ba75f1066b1f7427c2766287382bc
Todd Aspeotis <t@speot.is>
2013-07-01 21:50:45 +1000

    Adding Adminia Admin theme

--- Feed2Email/Controllers/AccountController.cs
+++ Feed2Email/Controllers/AccountController.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Feed2Email.Controllers
 {
     public class AccountController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Login()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult ResetPassword()
         {
             return View();
